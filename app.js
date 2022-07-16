0c7430beda7b7418cc322a7ed87897fdc5466ab4
birru <alanb_99@hotmail.com>
2022-07-15 21:46:19 -0600

    .

--- app.js
+++ app.js
@@ -14,16 +14,32 @@
 // }
 // console.log(incrementString("alan0"));
 
-function peopleWithAgeDrink(old) {
-  let drink = "";
-  if (old < 14) {
-    drink = "toddy";
-  } else if (old < 18) {
-    drink = "coke";
-  } else if (old < 21) {
-    drink = "beer";
-  } else if (old >= 21) {
-    drink = "whisky";
-  }
-  return `drink ${drink}`;
-}
+// function peopleWithAgeDrink(old) {
+//   let drink = "";
+//   if (old < 14) {
+//     drink = "toddy";
+//   } else if (old < 18) {
+//     drink = "coke";
+//   } else if (old < 21) {
+//     drink = "beer";
+//   } else if (old >= 21) {
+//     drink = "whisky";
+//   }
+//   return `drink ${drink}`;
+// }
+
+// function toAlternatingCase(str) {
+//   let arr = [];
+//   let arr_string = str.split("");
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i].toUpperCase() == str[i]) {
+//       arr.push(str[i].toLowerCase());
+//     } else if (str[i].toLowerCase() == str[i]) {
+//       arr.push(str[i].toUpperCase());
+//     }
+//   }
+//   return arr.join("");
+// }
+// console.log(toAlternatingCase("alan birRu"));
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {}
